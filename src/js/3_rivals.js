babba59d5a0d277d08a9bede11b551d0236f6878
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-03-20 19:42:39 +0200

    19:42

--- src/js/3_rivals.js
+++ src/js/3_rivals.js
@@ -1,7 +1,22 @@
 const rivalCar = document.getElementById('rival car');
 const rivalScale = document.querySelector('.rivals-circle-nav');
-const nodeRivalPoint = document.querySelectorAll('[data-way-point]');
+const nodeRivalScaleCircle = document.querySelectorAll('.nav-rivals-li');
+const rivalScaleCircle = Array.from(nodeRivalScaleCircle);
+const nodeRivalPoint = document.querySelectorAll('.chek-point-color');
 const rivalPoint = Array.from(nodeRivalPoint);
+const nodeCircleSvg = document.querySelectorAll('.rivals-case-svg');
+const circleSvg = Array.from(nodeCircleSvg);
+
+const nodeRivalModalWindows = document.querySelectorAll('.rivalsModal');
+const rivalModalWindow = Array.from(nodeRivalModalWindows);
+
+const nodeSiteName = document.querySelectorAll(
+  '[data-name="rivals-site-name"]',
+);
+const siteName = Array.from(nodeSiteName);
+
+console.log(rivalScaleCircle);
+
 var countMove = 0;
 
 rivalScale.onclick = function(event) {
@@ -10,27 +25,33 @@ rivalScale.onclick = function(event) {
 
   const countNumberCircle = Number(count);
   console.log(countNumberCircle);
-  /*   console.log(countNumberCircle);
-  console.log(countMove); */
   countMove = countNumberCircle;
   rivalCar.className = 'rivals-car move-car-fwd-section-' + countMove;
 };
 
 function rivalInfoMove(e) {
   if (e.name === 'rivalInfoRight') {
-    console.log('Вправо ' + countMove);
     if (countMove < 0) {
       countMove = 3;
-      console.log('Вправо ' + countMove);
     }
     if (countMove == 4) {
       countMove = 0;
-      console.log('Вправо ' + countMove);
     }
-    /*     rivalPoint[countMove].classList.remove('grey-point');
-    rivalPoint[countMove].classList.add('red-point'); */
-    countMove = countMove + 1;
-    rivalCar.className = 'rivals-car move-car-fwd-section-' + countMove;
+    if (countMove == 3) {
+      for (let z = 0; z < rivalPoint.length; z++) {
+        rivalPoint[z].classList.remove('red-point');
+        rivalPoint[z].classList.add('grey-point');
+      }
+      innerImgCase(0);
+      countMove = countMove + 1;
+      rivalCar.className = 'rivals-car move-car-fwd-section-' + countMove;
+    } else {
+      rivalPoint[countMove].classList.remove('grey-point');
+      rivalPoint[countMove].classList.add('red-point');
+      countMove = countMove + 1;
+      rivalCar.className = 'rivals-car move-car-fwd-section-' + countMove;
+      innerImgCase(countMove);
+    }
   }
   if (e.name === 'rivalInfoLeft') {
     if (countMove > 3) {
@@ -38,18 +59,53 @@ function rivalInfoMove(e) {
     }
     if (countMove < 0) {
       countMove = 3;
-      console.log('Влево ' + countMove);
     }
-    console.log('Влево ' + countMove);
 
-    rivalCar.className = 'rivals-car move-car-back-section-' + countMove;
-    countMove = countMove - 1;
-    console.log('Влево ' + countMove);
-    /*     rivalPoint[countMove].classList.remove('red-point');
-    rivalPoint[countMove].classList.add('grey-point'); */
+    if (countMove == 0) {
+      for (let z = 0; z < rivalPoint.length; z++) {
+        rivalPoint[z].classList.remove('grey-point');
+        rivalPoint[z].classList.add('red-point');
+      }
+      rivalCar.className = 'rivals-car move-car-back-section-' + countMove;
+
+      innerImgCase(3);
+      countMove = countMove - 1;
+    } else {
+      rivalCar.className = 'rivals-car move-car-back-section-' + countMove;
+      countMove = countMove - 1;
+      rivalPoint[countMove].classList.remove('red-point');
+      rivalPoint[countMove].classList.add('grey-point');
+      innerImgCase(countMove);
+    }
   }
 }
 
-/* function rivalInfoGo(e) {
-  rivalCar.className = 'rivals-car move-car-fwd-section-' + e;
-} */
+function innerImgCase(e) {
+  for (let a = 0; a < circleSvg.length; a++) {
+    circleSvg[a].classList.add('hide-case');
+    siteName[a].classList.add('hide-case');
+  }
+  setTimeout(function() {
+    for (let a = 0; a < circleSvg.length; a++) {
+      circleSvg[a].classList.add('hide');
+      siteName[a].classList.add('hide');
+      rivalScaleCircle[a].classList.remove('nav-li-active');
+    }
+  }, 1000);
+  setTimeout(function() {
+    circleSvg[e].classList.remove('hide', 'hide-case');
+    siteName[e].classList.remove('hide', 'hide-case');
+    rivalScaleCircle[e].classList.add('nav-li-active');
+  }, 1000);
+}
+
+function rivalModal() {
+  /*  console.log(rivalModalWindow.classList); */
+  rivalModalWindow[countMove].classList.remove('hide');
+  console.log(countMove);
+}
+
+function modalClose() {
+  rivalModalWindow[countMove].classList.add('hide');
+  console.log('Модалка - закрыта');
+}
